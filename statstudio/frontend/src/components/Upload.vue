f8c9629b8d0b28b02ad524d2fb2ba3abbe265981
gdberrio <guilherme.diazberrio@gmail.com>
2021-10-13 18:27:21 +0100

    1st version: upload csv component

---
+++ statstudio/frontend/src/components/Upload.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-file-input
+      counter
+      show-size
+      accept="text/csv"
+      label="file input"
+      placeholder="select csv to upload"
+      v-model="file"
+    >
+    </v-file-input>
+    <v-spacer></v-spacer>
+    <v-btn right @click="Import">Upload</v-btn>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      file: null,
+      data: null,
+    }
+  },
+  methods: {
+    Import() {
+      var reader = new FileReader();
+      reader.readAsText(this.file);
+      reader.onload = () => {
+        this.data = reader.result;
+      }
+    }
+  }
+}
+</script>
