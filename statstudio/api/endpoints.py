2c2f898f686285a50d0a7dec75ff470fabb81272
gdberrio <guilherme.diazberrio@gmail.com>
2021-10-17 16:36:01 +0100

    cleaned celery workers

--- statstudio/api/endpoints.py
+++ statstudio/api/endpoints.py
@@ -1,28 +1,12 @@
 import pandas as pd
 from fastapi import Body
 from fastapi.responses import JSONResponse
-from worker.celery_worker import create_task, create_task_count
+from worker.celery_worker import create_task_count
 from celery.result import AsyncResult
 import fastapi
 from statstudio.StatModels import CountDataPoisson
 
 router = fastapi.APIRouter()
-
-@router.post('/test_task')
-async def run_task(data=Body(...)):
-    delay = int(data["delay"])
-    x = data["x"]
-    y = data["y"]
-    task = create_task.delay(delay, x, y)
-    return {"task_id": str(task), "status": "Processing"}
-
-@router.get('/test_task/result/{task_id}')
-async def task_result(task_id):
-    task = AsyncResult(task_id)
-    if not task.ready():
-        return JSONResponse(status_code=202, content={'task_id': str(task_id), 'status': 'Processing'})
-    result = task.get()
-    return {'task_id': task_id, 'status': 'Success', 'result': str(result)}
 
 @router.post('/count_data/predict')
 async def run_count(data=Body(...)):
@@ -40,11 +24,3 @@ async def count_result(task_id):
         return JSONResponse(status_code=202, content={'task_id': str(task_id), 'status': 'Processing'})
     result = task.get()
     return {'task_id': task_id, 'status': 'Success', 'result': str(result)}
-
-@router.get('/')
-async def root():
-    return {"message": "Working with change!"}
-
-@router.get('/api/csv', status_code=fastapi.status.HTTP_200_OK)
-def get_csv():
-    pass
--- worker/celery_worker.py
+++ worker/celery_worker.py
@@ -12,11 +12,6 @@ celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL")
 celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND")
 
-@celery.task(name="test_task")
-def create_task(a, b, c):
-    time.sleep(a)
-    return b + c
-
 @celery.task(name="count_data")
 def create_task_count(data, data_column):
     tau_mean = CountModel(data=data, data_column=data_column)
